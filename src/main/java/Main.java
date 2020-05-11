ffeb88b99a2ed8156fd3eda4dd0227aba77be61d
Kirill <kirill.yudintsev@gmail.com>
2020-05-11 15:30:39 +0300

    Added receiving data from DB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+import org.bson.BsonDocument;
 import org.bson.Document;
 
 import java.io.FileReader;
@@ -38,7 +39,7 @@ public class Main {
 
         MongoCollection<Document> mongoStudents = testDB.getCollection("students");
 
-
+        mongoStudents.drop(); // в случае перезапуска программы, удаляем коллекцию, чтоб не дублировались данные
 
         for (Student student : students) { // добавляем в БД новые объекты
 
@@ -51,11 +52,65 @@ public class Main {
 
         }
 
-        FindIterable<Document> documents = mongoStudents.find();
-
-        System.out.println("Всего студентов: " + mongoStudents.countDocuments()); // получаем студентов из БД
+        System.out.println("Всего студентов: " + mongoStudents.countDocuments()); // получаем количество студентов из БД
 
 
+        /*
+          Выводим список студентов старше 40 лет с именами, возрастом и их курсами
+         */
+
+        BsonDocument mostElderStudentsQuery = BsonDocument.parse("{age: {$gt:40}}");
+
+        FindIterable<Document> studentsElderThan40years = mongoStudents.find(mostElderStudentsQuery);
+
+        System.out.println("Студенты старше 40 лет");
+
+        System.out.println(mongoStudents.countDocuments(mostElderStudentsQuery) + " чел.:");
+
+        for (Document doc : studentsElderThan40years) {
+
+            System.out.println(doc.get("name") + " " + doc.get("age") + " лет " + doc.get("courses"));
+
+        }
+
+        /*
+         * Имя самого молодого студента
+         */
+
+        FindIterable<Document> youngestStudentQuery = mongoStudents.find().sort(BsonDocument.parse("{age: 1}")).limit(1);
+        String youngestStudent = "";
+
+        for (Document doc : youngestStudentQuery) {
+            youngestStudent = doc.get("name").toString();
+        }
+
+        System.out.println("Самый молодой студент: " + youngestStudent);
+
+        /*
+        * Выводим имя и курсы самого возрастного студента
+        */
+
+        FindIterable<Document> eldestStudentQuery = mongoStudents.find().sort(BsonDocument.parse("{age: -1}")).limit(1);
+
+        String eldestStudentCourses = "";
+
+        for (Document doc : eldestStudentQuery) {
+            eldestStudentCourses = doc.get("courses").toString();
+        }
+
+        System.out.println("Cписок курсов самого возрастного студента: " + eldestStudentCourses);
+
+
+        /*
+         * Выводим имена самых молодых студентов
+         */
+        FindIterable<Document> youngestStudentsQuery = mongoStudents.find(BsonDocument.parse("{age: 18}"));
+
+
+        System.out.println("Имена самых молодых студентов: ");
+        for (Document doc : youngestStudentsQuery) {
+            String studentName = doc.get("name").toString() + " " + doc.get("age") + " лет";
+            System.out.println(studentName);
+        }
     }
-
 }
